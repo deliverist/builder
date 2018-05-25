a5d2cd0e9ad8a19928ab452ec1d996c57126aa93
Jan Pecha <janpecha@email.cz>
2018-05-25 12:06:46 +0200

    Commands: added GoogleClosureCompiler

---
+++ src/Commands/GoogleClosureCompiler.php
@@ -0,0 +1,58 @@
+<?php
+
+	namespace Deliverist\Builder\Commands;
+
+	use Deliverist\Builder\Builder;
+	use Deliverist\Builder\FileSystemException;
+	use Deliverist\Builder\InvalidArgumentException;
+	use Deliverist\Builder\ICommand;
+
+
+	class GoogleClosureCompiler implements ICommand
+	{
+		/**
+		 * @param  Builder
+		 * @param  string|string[]
+		 * @param  string|NULL
+		 */
+		public function run(Builder $builder, $files = NULL)
+		{
+			if (!isset($files)) {
+				throw new InvalidArgumentException("Missing parameter 'files'.");
+			}
+
+			if (!is_array($files)) {
+				$files = (array) $files;
+			}
+
+			foreach ($files as $file) {
+				$path = $builder->getPath($file);
+
+				if (!is_file($path)) {
+					throw new FileSystemException("File '$file' not found.");
+				}
+
+				$this->compressFile($path);
+			}
+		}
+
+
+		private function compressFile($path)
+		{
+			$content = file_get_contents($path);
+			$output = @file_get_contents('https://closure-compiler.appspot.com/compile', FALSE, stream_context_create(array(
+				'http' => array(
+					'method' => 'POST',
+					'header' => 'Content-type: application/x-www-form-urlencoded',
+					'content' => 'output_info=compiled_code&js_code=' . urlencode($content),
+				)
+			)));
+
+			if (!is_string($output)) {
+				$error = error_get_last();
+				throw new GoogleClosureCompilerException("Unable to minfy: {$error['message']}\n");
+			}
+
+			file_put_contents($path, $output);
+		}
+	}
---
+++ tests/Builder/Commands/GoogleClosureCompiler.phpt
@@ -0,0 +1,38 @@
+<?php
+
+use Deliverist\Builder\Builder;
+use Deliverist\Builder\Commands;
+use Tester\Assert;
+
+require __DIR__ . '/../../bootstrap.php';
+require __DIR__ . '/../../libs/TestBuilder.php';
+
+
+test(function () {
+
+	$log = array();
+	$builder = new TestBuilder(TEMP_DIR);
+	$builder->onLog[] = function ($message, $type) use (&$log) {
+		$log[] = array($message, $type);
+	};
+	$command = new Commands\GoogleClosureCompiler;
+
+	Assert::exception(function () use ($command, $builder) {
+		$command->run($builder);
+
+	}, 'Deliverist\Builder\InvalidArgumentException', "Missing parameter 'files'.");
+
+	Assert::exception(function () use ($command, $builder) {
+		$command->run($builder, 'not-found.js');
+
+	}, 'Deliverist\Builder\FileSystemException', "File 'not-found.js' not found.");
+
+	Assert::exception(function () use ($command, $builder) {
+		$command->run($builder, array(
+			'not-found-1.js',
+			'not-found-2.js',
+		));
+
+	}, 'Deliverist\Builder\FileSystemException', "File 'not-found-1.js' not found.");
+
+});
