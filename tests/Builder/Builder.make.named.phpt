d6b7e18db8ad2767471e269d38842534b5b8d0e7
Jan Pecha <janpecha@email.cz>
2025-06-20 21:40:56 +0200

    Added declare(strict_types=1)

--- tests/Builder/Builder.make.named.phpt
+++ tests/Builder/Builder.make.named.phpt
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * @phpVersion >= 8.0
  */
--- tests/libs/TestBuilder.php
+++ tests/libs/TestBuilder.php
@@ -1,5 +1,7 @@
 <?php
 
+	declare(strict_types=1);
+
 	class TestBuilder extends Deliverist\Builder\Builder
 	{
 		/**
